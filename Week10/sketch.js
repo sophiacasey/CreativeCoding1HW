533d57ef7562dc4051293b51adc5c68c0d9d0ca9
sophiacasey <sophia.casey@umontana.edu>
2024-03-31 11:26:51 -0600

    example project

--- Week10/sketch.js
+++ Week10/sketch.js
@@ -1,17 +1,68 @@
-var redColor = 123;
-var greenColor = 39;
-var blueColor = 21;
- // this function is called only once
+//self portrait//
 function setup()
 {
-    createCanvas(800,600);
+    createCanvas(500, 600);
 }
-/* this function is called continuously
-    while the sketch is open in the browser
-*/
+
+// hello!
 function draw()
 {
- // change the background color by updating the variables at the top  
-  background(redColor,greenColor,blueColor);
- 
+    background(209,45,78);
+    textSize(22)
+    text("Hello!", 8,70);
+
+    // head
+    fill(255, 190, 150);
+    circle(250,120,200);
+   
+    // eyes
+    strokeWeight(20);
+    fill(0);
+    point(200,90);
+    point(285,90);
+
+    // nose
+    strokeWeight(10);
+    point(245,115);
+    
+    // mouth
+    strokeWeight(10);
+    ellipse(245, 150, 50, 20)
+
+    // hair
+    line(110,230,175,50);
+    line(330,50,360,230);
+
+    // body
+    fill(50, 50, 100);
+    rect(200,210,100,170);
+    
+    // decoration
+    fill(500);
+    triangle(205,380,250,223,295,380)
+
+    // right arm
+    fill(10, 24, 120);
+    rect(300,220,50,10);
+
+    // left arm
+    rect(150,220,50,10);
+
+    // left leg
+    rect(200,390,10,50);
+    
+    // right leg
+    rect(290,390,10,50);
+    
+    fill(120);
+    textSize(size);
+    size+= sizeDirection;
+    count++;
+    if(count > 5)
+    {
+        sizeDirection *=-1;
+        count = 0;
+    }
+    text("Sophia Casey",270,500 );
+
 }
